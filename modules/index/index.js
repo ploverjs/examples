1a9746002b38ac4d6a53362b77c2a286ff02bedf
bencode <bencode@163.com>
2016-05-05 11:49:15 +0800

    feat: 完成首页

--- modules/index/index.js
+++ modules/index/index.js
@@ -1,7 +1,15 @@
 'use strict';
 
 
-exports.view = function() {
+const request = require('request');
+const pathUtil = require('path');
+const resolveFrom = require('resolve-from');
+
+
+exports.view = function* () {
+  const root = this.settings.applicationRoot;
+  const modules = yield* getModuleList(root);
+
   this.layout = {
     data: {
       title: 'Plover应用示例'
@@ -9,8 +17,37 @@ exports.view = function() {
   };
 
   const data = {
-    desc: 'Plover - 专注于模块化的NodeJs Web框架'
+    desc: 'Plover - 专注于模块化的NodeJs Web框架',
+    modules: modules
   };
 
   this.render(data);
 };
+
+
+
+const apiHost = 'http://registry.npmjs.com';    // /plover/latest
+
+function* getModuleList(root) {
+  const pkg = require(pathUtil.join(root, 'package.json'));
+  const list = Object.keys(pkg.dependencies).map(function(name) {
+    const path = resolveFrom(root, name + '/package.json');
+    const info = require(path);
+    const apiUrl = `${apiHost}/${name}/latest`;
+    return new Promise(resolve => {
+      request(apiUrl, (error, response, body) => {
+        if (error) {
+          resolve({});
+        }
+
+        const o = {
+          name: name,
+          local: info,
+          remote: JSON.parse(body)
+        };
+        resolve(o);
+      });
+    });
+  });
+  return yield list;
+}

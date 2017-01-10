cd4f47bd231fa81d03dd3e921306f19ccc078ee0
bencode <bencode@163.com>
2017-01-10 23:11:51 +0800

    add edit page

--- routes/modules/books/index.js
+++ routes/modules/books/index.js
@@ -6,7 +6,7 @@ exports.index = function* () {
 
 exports.show = function* () {
   const id = parseInt(this.query.id, 10);
-  const book = yield this.Book.get(id);
+  const book = yield getBook(this);
 
   if (!book) {
     this.throw(404, `can not find book #{id}`);
@@ -17,4 +17,17 @@ exports.show = function* () {
   this.layout.data.pageTitle = book.name;
 
   this.render({ book });
+};
+
+
+exports.edit = function* () {
+  const book = yield getBook(this);
+  this.layout.data.pageTitle = 'Edit: ' + book.name;
+  this.render({ book });
+};
+
+
+function* getBook(ctx) {
+  const id = parseInt(ctx.query.id, 10);
+  return yield ctx.Book.get(id);
 }

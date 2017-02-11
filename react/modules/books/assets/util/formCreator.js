ef499eeda0651c98739cc1cdcd45c280eebd7989
bencode <bencode@163.com>
2017-02-11 22:19:08 +0800

    add formCreator

---
+++ react/modules/books/assets/util/formCreator.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+
+export default function formCreator(Form) {
+  class FormWrapper extends Component {
+    static propTypes = {
+      formData: PropTypes.object,
+      onSubmit: PropTypes.func
+    }
+
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        formData: Object.assign({}, props.formData);
+      };
+    }
+
+
+    onSubmit() {
+      const data = Object.assign({}, this.state.formData);
+      const { onSubmit } = this.props;
+      onSubmit(data);
+    }
+
+
+    render() {
+      const updater = (name, v) => {
+        return (e) => {
+          const value = v && v() || e.target.value;
+          const formData = Object.assign({}, this.state.formData);
+          formData[name] = value;
+          this.setState({ formData });
+        };
+      };
+
+      return (
+        <Form formData={this.state.formData}
+            updater={updater} onSubmit={::this.onSubmit} />
+      );
+    }
+  }
+}

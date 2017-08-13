5a8cd7b8d458548bb4aea75adee1319972a53232
bencode <bencode@163.com>
2017-08-13 13:19:02 +0800

    upgrade ploverx

--- static/config/app.js
+++ static/config/app.js
@@ -9,6 +9,11 @@ exports.server = {
 exports.web = {
   keys: ['99D4ABFE-B032-4D68-A616-F68FADB4BE2F'],
 
+  rtime: {},
+  conditional: {},
+  compress: {},
+  etag: {},
+
   // koa-static配置参考：
   // https://github.com/koajs/static
   static: {
